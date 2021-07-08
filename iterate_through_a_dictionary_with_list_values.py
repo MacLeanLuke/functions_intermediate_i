b7ec11db32247aa9822150485a16e7ffdf4e0bc9
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-07-07 20:26:03 -0500

    initial commit

---
+++ iterate_through_a_dictionary_with_list_values.py
@@ -0,0 +1,35 @@
+# Iterate Through a Dictionary with List Values
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for k in some_dict:
+        print(str(len(some_dict[k])) + ' ' + k)
+        for j in range(0, len(some_dict[k])):
+            print(some_dict[k][j])
+       
+printInfo(dojo)
+
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
